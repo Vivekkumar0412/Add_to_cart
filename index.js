3212ff6a27962382d80614fcff9d0aabf4e637f3
Vivekkumar0412 <www.vivekkumar0412@gmail.com>
2023-05-12 13:12:18 +0530

    a new array is added

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-alert("hi");
+let products = [
+  { prod: "Pepsi", price: 15, stock: 20 },
+  { prod: "Coke", price: 18, stock: 30 },
+  { prod: "Maggi", price: 10, stock: 35 },
+  { prod: "Nescafe", price: 40, stock: 10 },
+  { prod: "Dairy Milk", price: 30, stock: 12 },
+  { prod: "Frito Lays", price: 20, stock: 15 },
+];
